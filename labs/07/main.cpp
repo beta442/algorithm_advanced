736e5d749c696ebcc1efe6c614b3856ad18ba6e1
beta442 <grandoblivioniv@gmail.com>
2022-04-27 23:15:09 +0300

    num_of_divisors: refactor

--- labs/07/main.cpp
+++ labs/07/main.cpp
@@ -17,7 +17,12 @@
  * Вывод в файл OUTPUT.TXT. В выходном файле должно содержаться одно число — количество нормальных наборов из k делителей числа N.
  */
 
-#include "headers/pch.h"
+#include <fstream>
+#include <functional>
+#include <iostream>
+#include <optional>
+#include <string>
+#include <vector>
 
 struct Args
 {
@@ -75,6 +80,31 @@ ParsedArgs ParseArgs(std::ifstream& inputFile, std::ofstream& outputFile)
 	return parsedArgs;
 }
 
+std::vector<int> GetNumberDivisors(const int number)
+{
+	std::vector<int> result;
+
+	int sqrtN = static_cast<int>(std::floor(std::sqrt(number)));
+	for (int d = 1; d <= sqrtN; ++d)
+	// получим список делителей до корня N
+	{
+		if (number % d == 0)
+		{
+			result.push_back(d);
+		}
+	}
+	for (int i = static_cast<int>(std::size(result) - 1); i >= 0; --i)
+	// получим список делителей (от корня N) до (N)
+	{
+		if (result[i] * result[i] != number)
+		{
+			result.push_back(number / result[i]);
+		}
+	}
+
+	return result;
+}
+
 int GCD(int x, int y)
 {
 	if (x == INT_MIN || y == INT_MIN)
@@ -92,6 +122,53 @@ int GCD(int x, int y)
 	return (y != 0) ? y : 1;
 }
 
+std::vector<std::vector<bool>> GetCoprimeMatrixOfIntVector(const std::vector<int>& vec)
+{
+	int divisorsAmount = static_cast<int>(std::size(vec));
+	std::vector<std::vector<bool>> coprime(divisorsAmount, std::vector<bool>(divisorsAmount));
+	for (int i = 0; i < divisorsAmount; ++i)
+	// создадим матрицу, по которой можно будет проверить - являются ли числа взаимнопростыми
+	{
+		for (int j = i; j < divisorsAmount; ++j)
+		{
+			coprime[i][j] = GCD(vec[i], vec[j]) == 1;
+			coprime[j][i] = coprime[i][j];
+		}
+	}
+
+	return coprime;
+}
+
+std::vector<std::vector<double>> GetMinMulProductOfIntVector(const std::vector<int>& vec,
+	const int amountOfNumbers, const int upperNumber)
+{
+	int divisorsAmount = static_cast<int>(std::size(vec));
+	std::vector<std::vector<double>> minProduct(amountOfNumbers + 1, std::vector<double>(divisorsAmount + 1));
+	for (int i = 1; i <= amountOfNumbers; ++i)
+	{
+		for (int j = 0; j <= divisorsAmount; ++j)
+		{
+			if (j == divisorsAmount)
+			// базовый случай
+			{
+				minProduct[i][j] = upperNumber + 1;
+			}
+			else if (i == 1)
+			// базовый случай
+			{
+				minProduct[i][j] = vec[j];
+			}
+			else
+			// заполняем
+			{
+				minProduct[i][j] = minProduct[i - 1][j + 1] * vec[j];
+			}
+		}
+	}
+
+	return minProduct;
+}
+
 void PrintCoprimeNumbersNormalizedSequenceOfN(const int n, const short amountOfNumbers, std::ofstream& toFile)
 {
 	if (n <= 0 || amountOfNumbers <= 0)
@@ -99,69 +176,31 @@ void PrintCoprimeNumbersNormalizedSequenceOfN(const int n, const short amountOfN
 		toFile << "N and k should be positive natural numbers" << std::endl;
 		return;
 	}
-	std::vector<int> divisors;
 
-	int sqrtN = static_cast<int>(std::floor(std::sqrt(n)));
-	for (int d = 1; d <= sqrtN; ++d)
-	// получим список делителей до корня N
-	{
-		if (n % d == 0)
-		{
-			divisors.push_back(d);
-		}
-	}
-	for (int i = static_cast<int>(std::size(divisors) - 1); i >= 0; --i)
-	// получим список делителей (от корня N) до (N)
-	{
-		if (divisors[i] * divisors[i] != n)
-		{
-			divisors.push_back(n / divisors[i]);
-		}
-	}
+	// получим список делителей числа n
+	std::vector<int> divisors = GetNumberDivisors(n);
 
+	// "взаимнопростые" соответствие в виде матрицы, где индекс строки/столбца coprime соответствует индексу элемента divisors
+	std::vector<std::vector<bool>> coprime = GetCoprimeMatrixOfIntVector(divisors);
+
+	// получим матрицу, с помощью которой сможем собирать произведениями число вплоть до N, проверять: а можем ли мы домножить дальше?
+	std::vector<std::vector<double>> minProduct = GetMinMulProductOfIntVector(divisors, amountOfNumbers, n);
+
+	int total = 0; // счётчик набранных наборов
 	int divisorsAmount = static_cast<int>(std::size(divisors));
-	std::vector<std::vector<bool>> coprime(divisorsAmount, std::vector<bool>(divisorsAmount));
-	for (int i = 0; i < divisorsAmount; ++i)
-	// создадим матрицу, по которой можно будет проверить - являются ли числа взаимнопростыми
-	{
-		for (int j = i; j < divisorsAmount; ++j)
-		{
-			coprime[i][j] = GCD(divisors[i], divisors[j]) == 1;
-			coprime[j][i] = coprime[i][j];
-		}
-	}
-
-	std::vector<std::vector<double>> minProduct(amountOfNumbers + 1, std::vector<double>(divisorsAmount + 1));
-	for (int i = 1; i <= amountOfNumbers; ++i)
-		// создадим матрицу, по которой сможем определить минимальный результат умножения двух делителей
-	{
-		for (int j = 0; j <= divisorsAmount; ++j)
-		{
-			if (j == divisorsAmount)
-			{
-				minProduct[i][j] = n + 1;
-			}
-			else if (i == 1)
-			{
-				minProduct[i][j] = divisors[j];
-			}
-			else
-			{
-				minProduct[i][j] = minProduct[i - 1][j + 1] * divisors[j];
-			}
-		}
-	}
-
-	int total = 0;
 	std::function<void(int, int, int)> count = [&](int lastDividerIndex, int currDivider, int numbersLeft) {
 		if (numbersLeft == 0)
 		{
 			total++;
 			return;
 		}
-		for (int currDividerIndex = lastDividerIndex + 1; currDividerIndex < divisorsAmount && currDivider * minProduct[numbersLeft][currDividerIndex] <= n; ++currDividerIndex)
+		for (int currDividerIndex = lastDividerIndex + 1;
+			currDividerIndex < divisorsAmount && // перебираем все делители
+			currDivider * minProduct[numbersLeft][currDividerIndex] <= n; // можно ли умножать дальше?
+			++currDividerIndex)
 		{
 			if (coprime[lastDividerIndex][currDividerIndex])
+				// подобрали делитель, считаем дальше
 			{
 				count(currDividerIndex, currDivider * divisors[currDividerIndex], numbersLeft - 1);
 			}
